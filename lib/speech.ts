9398ceb15a987f77381af200ae2f45a8ce40115e
Mishe kon <konmishe9@gmail.com>
2026-09-13 21:11:18 +0100

    Fix speech support module

--- lib/speech.ts
+++ lib/speech.ts
@@ -1,81 +1,94 @@
-interface SpeechRecognitionAlternative {
-  readonly transcript: string;
-  readonly confidence: number;
-}
+// interface SpeechRecognitionAlternative {
+//   readonly transcript: string;
+//   readonly confidence: number;
+// }
 
-interface SpeechRecognitionResult {
-  readonly isFinal: boolean;
-  readonly length: number;
-  item(index: number): SpeechRecognitionAlternative;
-  [index: number]: SpeechRecognitionAlternative;
-}
+// interface SpeechRecognitionResult {
+//   readonly isFinal: boolean;
+//   readonly length: number;
+//   item(index: number): SpeechRecognitionAlternative;
+//   [index: number]: SpeechRecognitionAlternative;
+// }
 
-interface SpeechRecognitionResultList {
-  readonly length: number;
-  item(index: number): SpeechRecognitionResult;
-  [index: number]: SpeechRecognitionResult;
-}
+// interface SpeechRecognitionResultList {
+//   readonly length: number;
+//   item(index: number): SpeechRecognitionResult;
+//   [index: number]: SpeechRecognitionResult;
+// }
 
-interface SpeechRecognitionEvent extends Event {
-  readonly resultIndex: number;
-  readonly results: SpeechRecognitionResultList;
-}
+// interface SpeechRecognitionEvent extends Event {
+//   readonly resultIndex: number;
+//   readonly results: SpeechRecognitionResultList;
+// }
 
-interface SpeechRecognitionErrorEvent extends Event {
-  readonly error: string;
-  readonly message: string;
-}
+// interface SpeechRecognitionErrorEvent extends Event {
+//   readonly error: string;
+//   readonly message: string;
+// }
 
-interface SpeechGrammar {
-  src: string;
-  weight: number;
-}
+// interface SpeechGrammar {
+//   src: string;
+//   weight: number;
+// }
 
-interface SpeechGrammarList {
-  readonly length: number;
-  item(index: number): SpeechGrammar;
-  addFromString(string: string, weight?: number): void;
-  addFromURI(src: string, weight?: number): void;
-  [index: number]: SpeechGrammar;
-}
+// interface SpeechGrammarList {
+//   readonly length: number;
+//   item(index: number): SpeechGrammar;
+//   addFromString(string: string, weight?: number): void;
+//   addFromURI(src: string, weight?: number): void;
+//   [index: number]: SpeechGrammar;
+// }
 
-interface SpeechRecognition extends EventTarget {
-  continuous: boolean;
-  grammars: SpeechGrammarList;
-  interimResults: boolean;
-  lang: string;
-  maxAlternatives: number;
-  serviceURI: string;
+// interface SpeechRecognition extends EventTarget {
+//   continuous: boolean;
+//   grammars: SpeechGrammarList;
+//   interimResults: boolean;
+//   lang: string;
+//   maxAlternatives: number;
+//   serviceURI: string;
 
-  onaudiostart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onaudioend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onerror:
-    | ((this: SpeechRecognition, ev: SpeechRecognitionErrorEvent) => unknown)
-    | null;
-  onnomatch:
-    | ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => unknown)
-    | null;
-  onresult:
-    | ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => unknown)
-    | null;
-  onsoundstart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onsoundend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onspeechstart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onspeechend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
-  onstart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onaudiostart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onaudioend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onerror:
+//     | ((this: SpeechRecognition, ev: SpeechRecognitionErrorEvent) => unknown)
+//     | null;
+//   onnomatch:
+//     | ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => unknown)
+//     | null;
+//   onresult:
+//     | ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => unknown)
+//     | null;
+//   onsoundstart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onsoundend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onspeechstart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onspeechend: ((this: SpeechRecognition, ev: Event) => unknown) | null;
+//   onstart: ((this: SpeechRecognition, ev: Event) => unknown) | null;
 
-  abort(): void;
-  start(): void;
-  stop(): void;
-}
+//   abort(): void;
+//   start(): void;
+//   stop(): void;
+// }
 
-declare var SpeechRecognition: {
-  prototype: SpeechRecognition;
-  new (): SpeechRecognition;
-};
+// declare var SpeechRecognition: {
+//   prototype: SpeechRecognition;
+//   new (): SpeechRecognition;
+// };
 
-interface Window {
-  SpeechRecognition?: typeof SpeechRecognition;
-  webkitSpeechRecognition?: typeof SpeechRecognition;
+// interface Window {
+//   SpeechRecognition?: typeof SpeechRecognition;
+//   webkitSpeechRecognition?: typeof SpeechRecognition;
+// }
+
+
+
+export function isSpeechRecognitionSupported(): boolean {
+  if (typeof window === 'undefined') {
+    return false;
+  }
+
+  return (
+    'SpeechRecognition' in window ||
+    'webkitSpeechRecognition' in window
+  );
 }
